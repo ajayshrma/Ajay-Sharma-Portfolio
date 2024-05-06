50fbbe3b2358cf56eaeb186ed17bf6654afdba37
ajayshrma <ajaysharma27600@gmail.com>
2024-05-06 22:55:32 +0530

    index.js updated

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
 
 const toggleNavbar = () => {
-  // alert("Plz Subscribe ");
   nav_header.classList.toggle("active");
 };
 
-mobile_nav.addEventListener("click", () => toggleNavbar());
+mobile_nav.addEventListener("click", toggleNavbar);
 
-// Navigation btn
+// Function to close mobile menu when a navigation link is clicked
+const closeMobileMenu = () => {
+  if (nav_header.classList.contains("active")) {
+    nav_header.classList.remove("active");
+  }
+};
 
+// Add event listeners to each navigation link inside the mobile menu
+document.querySelectorAll(".navbar-list").forEach((link) => {
+  link.addEventListener("click", closeMobileMenu);
+});
+
+// Smooth scrolling for navigation links
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -23,6 +33,22 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+// Navigation btn
+
+// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
+
+//     const headerHeight = document.querySelector("header").offsetHeight;
+//     const target = document.querySelector(this.getAttribute("href"));
+
+//     window.scrollTo({
+//       top: target.offsetTop - headerHeight,
+//       behavior: "smooth",
+//     });
+//   });
+// });
 
 // resume  download btn
 
